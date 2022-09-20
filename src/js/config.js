852f2b6d4edf402cb78a5237437f18ee2330c41b
UniqueMemory <jtkachuk11@gmail.com>
2022-09-19 20:39:35 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2fcac6e0-4f6a-4214-a7a0-2bb57cdbc13b';
+export const MODAL_CLOSE_SEC = 2.5;
